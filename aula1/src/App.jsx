0c57692d2b792a211ec6ead18971903de9fc550a
ThayJarosz <147217212+ThayJarosz@users.noreply.github.com>
2024-02-04 09:05:08 -0300

    commit inicial

---
+++ aula1/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Cabecalho from './Cabecalho'
+
+function App() {
+
+
+  return (
+    <>
+      <Cabecalho />
+    </>
+  )
+}
+
+export default App
---
+++ aula1/src/Cabecalho.jsx
@@ -0,0 +1,14 @@
+export default function Cabecalho() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#news">Novidades</a></li>
+                    <li><a href="#onSale">Ofertas</a></li>
+                    <li><a href="#about">Sobre</a></li>
+                    <li><a href="#subscriptions">Newsletter</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
